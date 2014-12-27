ea4aecb7d2b67387bcf64d4abd71241de2542e9c
radoslavCvijetinovic <rajkofon@yahoo.com>
2014-12-27 18:46:20 +0100

    successive refinement

---
+++ clean.code.successive.refinement/src/clean/code/successive/refinement/first/draft/ArgsException.java
@@ -0,0 +1,11 @@
+package clean.code.successive.refinement.first.draft;
+
+public class ArgsException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public String errorMessage() {
+		return getMessage();
+	}
+	
+	
+}
